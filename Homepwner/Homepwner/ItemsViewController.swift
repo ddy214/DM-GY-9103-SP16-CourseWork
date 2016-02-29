288a4468d36602ab1d6a8147c3c9c818ded1a068
ddy214 <ddy214@nyu.edu>
2016-02-28 23:56:47 -0500

    chapter 9 and 10"
    "

---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Derek Yu on 2/27/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController:UITableViewController{
+    
+    @IBAction func addNewItem(sender:AnyObject){
+//        let lastRow = tableView.numberOfRowsInSection(0)
+//        let indexPath = NSIndexPath(forRow: lastRow, inSection: 0)
+//        
+//        tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+//        
+        let newItem = itemStore.createItem()
+        if let index = itemStore.allItems.indexOf(newItem){
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+        
+
+    }
+    
+    @IBAction func toggleEditingMode(sender:AnyObject){
+        if editing{
+            sender.setTitle("Edit", forState: .Normal)
+            setEditing(false, animated: true)
+            
+        }
+        else{
+            sender.setTitle("Done", forState: .Normal)
+            setEditing(true, animated: true)
+        }
+    }
+    var itemStore:ItemStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete{
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title:title, message:message,preferredStyle: .ActionSheet)
+            
+            let cancelAction = UIAlertAction(title:"Cancel", style: .Cancel, handler:nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title:"Delete", style: .Destructive, handler:{(action) -> Void in
+                self.itemStore.removeItem(item)
+                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            })
+            ac.addAction(deleteAction)
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+}
